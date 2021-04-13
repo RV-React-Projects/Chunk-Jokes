b0672a1442c9752d88483a6c904242d1102cce72
Ranvijay Kumar <67736054+RanvijayChouhan12@users.noreply.github.com>
2021-04-13 22:46:59 +0530

    Initial Commit
    First Commit after the Final Change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+export default class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			list: [],
+			index: [],
+			buttonValue: '',
+			mainjoke: [],
+			joke: ''
+		};
+	}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	componentDidMount() {
+		fetch('https://api.chucknorris.io/jokes/categories').then((response) => response.json()).then((data) => {
+			this.setState({ list: data });
+		});
+	}
+	render() {
+		const randomjokes = (e) => {
+			fetch(`https://api.chucknorris.io/jokes/random?${e}/value`).then((response) => response.json()).then((data) => {
+				this.setState({ mainjoke: data.value });
+			});
+		};
+
+		const JokePage = () => {
+			const but = this.state.buttonValue;
+			const joke = this.state.mainjoke;
+			return (
+				<div className="Joke__Box">
+					<h2 className="Joke__Title">
+						Selected Category : <span>{but}</span>{' '}
+					</h2>
+					<div className="joke__Area" key={this.state.mainjoke.index}>
+						<h3 className="Joke__Mian" key={this.state.mainjoke.index}>
+							{joke}
+						</h3>
+					</div>
+					<button
+						className="new__joke"
+						key={but.length}
+						name={but}
+						value={but}
+						type="submit"
+						onClick={(e) => {
+							this.setState({ buttonValue: e.target.value });
+							randomjokes(this.state.buttonValue);
+						}}
+					>
+						New Joke
+					</button>
+				</div>
+			);
+		};
+		return (
+			<div className="mainContainer">
+				<h1>Chuck Norris jokes website.</h1>
+				<div className="buttonContaines">
+					{this.state.list.map((item) => (
+						<div className="all__buttons" key={item.index}>
+							<button
+								key={item.index}
+								name={item}
+								value={item}
+								className="mainButton"
+								type="submit"
+								onClick={(e) => {
+									this.setState({ buttonValue: e.target.value });
+									randomjokes(this.state.buttonValue);
+								}}
+							>
+								{item}
+							</button>
+						</div>
+					))}
+				</div>
+				<JokePage />
+			</div>
+		);
+	}
 }
-
-export default App;
